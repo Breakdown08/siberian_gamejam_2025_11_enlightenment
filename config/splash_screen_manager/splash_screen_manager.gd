2465b39457c839e5043d7ac52b129e4d9d413fec
DeNick <disar2014@gmail.com>
2025-11-10 21:45:01 +0700

    Добавлен скип заставок и анимация глаза

--- config/splash_screen_manager/splash_screen_manager.gd
+++ config/splash_screen_manager/splash_screen_manager.gd
@@ -13,33 +13,63 @@ extends Control
 @export var eye4 : TextureRect
 @export var eye5 : TextureRect
 
-func _ready() -> void:
-	fade()
+@onready var EyeAnim = $control/EyeAnim
+var timer
 
-func fade() -> void:
-	splash_screen.modulate.a = 0.0
-	var tween = self.create_tween()
-	tween.tween_interval(in_time)
-	tween.tween_property(splash_screen, "modulate:a", 1.0, fade_in_time)
-	tween.tween_interval(pause_time)
-	tween.tween_property(splash_screen, "modulate:a", 0.0, fade_out_time)
-	tween.tween_interval(out_time)
-	
-	eye1.modulate.a = 0.0
-	tween.tween_interval(in_time)
-	tween.tween_property(eye1, "modulate:a", 1.0, fade_in_time)
-	tween.tween_interval(pause_time)
-	tween.tween_property(eye1, "modulate:a", 0.0, fade_out_time)
-	tween.tween_interval(out_time)
-	
-	await tween.finished
+var tween
+var splash_scene
+var loaded
+
+func _ready() -> void:
+	#Загружаем сцену с меню
 	var loader = ResourceLoader.load_threaded_request(load_scene_path)
 	if loader == null:
 		push_error("Не удалось создать ResourceInteractiveLoader!")
 		return
+	loaded = ResourceLoader.load_threaded_get(load_scene_path) as PackedScene
 	
-	var loaded = ResourceLoader.load_threaded_get(load_scene_path) as PackedScene
+	#Какие-то костыли с заставками
+	fade(1)
+	timer = get_tree().create_timer(5)
+	await timer.timeout
+	#await tween.finished
+	timer = get_tree().create_timer(2.4)
+	fade(2)
+	#await tween.finished
+	await timer.timeout  # ждём пока таймер сработает
+	
+	#Меняем сцену на меню и удаляем эту сцену
+	change_scene()
+
+
+func change_scene():
 	if loaded:
 		get_tree().change_scene_to_packed(loaded)
+		self.queue_free() 
 	else:
 		push_error("Не удалось получить загруженную сцену.")
+
+func fade(step):
+	splash_scene = step
+	if splash_scene == 1:
+		tween = self.create_tween()
+		splash_screen.modulate.a = 0.0
+		tween.tween_interval(in_time)
+		tween.tween_property(splash_screen, "modulate:a", 1.0, fade_in_time)
+		tween.tween_interval(pause_time)
+		tween.tween_property(splash_screen, "modulate:a", 0.0, fade_out_time)
+		tween.tween_interval(out_time)
+	
+	if splash_scene == 2:
+		EyeAnim.play("Blink")
+
+func _input(event):
+	if (event is InputEventKey or event is InputEventMouseButton) and event.pressed:
+		if splash_scene == 1:
+			#tween.stop()
+			timer.set_time_left(0)
+			fade(splash_scene + 1)
+		elif splash_scene == 2:
+			#tween.stop()
+			timer.set_time_left(0)
+			change_scene()
