1bac33b2844b02448dca71bc2633fdd14839a196
DeNick <disar2014@gmail.com>
2025-11-14 23:40:47 +0700

    fix: rebase to dev

--- config/splash_screen_manager/splash_screen_manager.gd
+++ config/splash_screen_manager/splash_screen_manager.gd
@@ -1,6 +1,6 @@
 extends Control
 
-@export var load_scene_path : String = "res://config/main.tscn"  # путь к сцене
+@export var load_scene_path : String = "res://config/main.tscn"
 @export var in_time : float = 0.5
 @export var fade_in_time : float = 1.5
 @export var pause_time : float = 1.5
@@ -13,32 +13,29 @@ extends Control
 @export var eye4 : TextureRect
 @export var eye5 : TextureRect
 
-@onready var EyeAnim = $control/EyeAnim
-var timer
+@onready var eye_anim = $control/eye_anim
 
+var timer
 var tween
 var splash_scene
 var loaded
 
 func _ready() -> void:
-	#Загружаем сцену с меню
 	var loader = ResourceLoader.load_threaded_request(load_scene_path)
 	if loader == null:
 		push_error("Не удалось создать ResourceInteractiveLoader!")
 		return
 	loaded = ResourceLoader.load_threaded_get(load_scene_path) as PackedScene
 	
-	#Какие-то костыли с заставками
+	#Реализация начальных заставок
 	fade(1)
 	timer = get_tree().create_timer(5)
 	await timer.timeout
-	#await tween.finished
+	
 	timer = get_tree().create_timer(2.4)
 	fade(2)
-	#await tween.finished
-	await timer.timeout  # ждём пока таймер сработает
+	await timer.timeout
 	
-	#Меняем сцену на меню и удаляем эту сцену
 	change_scene()
 
 
@@ -61,16 +58,13 @@ func fade(step):
 		tween.tween_interval(out_time)
 	
 	if splash_scene == 2:
-		EyeAnim.play("Blink")
+		eye_anim.play("blink")
 
 func _input(event):
 	if (event is InputEventKey or event is InputEventMouseButton) and event.pressed:
+		timer.set_time_left(0)
 		if splash_scene == 1:
-			#tween.stop()
 			splash_screen.visible = false
-			timer.set_time_left(0)
 			fade(splash_scene + 1)
 		elif splash_scene == 2:
-			#tween.stop()
-			timer.set_time_left(0)
 			change_scene()
